11740d04aae7f5a5bc4d3d39983d73446e2903da
samtsmith <samtsmith@gmail.com>
2022-01-11 12:48:18 -0600

    Assignment 1

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().id(customerId)
+                .name("Sam Smith")
+                .build();
+    }
+}
+

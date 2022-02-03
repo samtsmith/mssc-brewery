0f902106155833faaa1202010e5a1639a10c680e
Sam Smith <samtsmith@gmail.com>
2022-02-03 16:14:14 +0000

    Corrected the validation of CustomerDto.name to also catch blanks

--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 import java.util.UUID;
@@ -20,7 +18,7 @@ public class CustomerDto {
     @Null
     private UUID id;
 
-    @NotNull
+    @NotBlank
     @Size(min=3, max=100)
     private String name;
 }

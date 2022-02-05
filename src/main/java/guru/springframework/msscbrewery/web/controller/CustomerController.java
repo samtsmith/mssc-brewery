f275b9b59384960428e215d3c4fd83cccf8786f0
Sam Smith <samtsmith@gmail.com>
2022-02-05 18:58:45 +0000

    Non-functional. Cleaning up some stuffs

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -5,17 +5,10 @@ import guru.springframework.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RequestMapping("/api/v1/customer")
 @RestController
